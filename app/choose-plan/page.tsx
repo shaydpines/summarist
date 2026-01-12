88bb61a9fc7405547a18656213c1de7956751fb2
shaydpines <oceanrooster@gmail.com>
2026-01-11 20:55:26 -0500

    header for choose-plan styled

--- app/choose-plan/page.tsx
+++ app/choose-plan/page.tsx
@@ -3,20 +3,27 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="plan">
-      <div className="plan__header--wrapper">
-        <div className="plan__header">
-          <div className="plan__title">
+    <div className="plan w-full">
+      <div className="plan__header--wrapper relative text-center w-full pt-12 mb-6 before:content[''] before:rounded-b-none md:before:rounded-b-full before:absolute before:top-0 before:left-0 before:z-[-1] before:w-full before:h-full before:bg-[#032b41]">
+        <div className="plan__header max-w-[1000px] my-0 mx-auto text-white px-6">
+          <div className="plan__title text-[26px] md:text-[48px] mb-8 md:mb-10 font-bold ">
             Get unlimited access to many amazing books to read
           </div>
-          <div className="plan__sub--title">
+          <div className="plan__sub--title text-[16px] md:text-[20px] mb-8">
             Turn ordinary moments into amazing learning opportunities
           </div>
-          <figure className="plan__img--mask"></figure>
+          <figure className="plan__img--mask flex justify-center max-w-[340px] my-0 mx-auto rounded-t-full overflow-hidden">
+            <Image
+              src="/images/pricing-top.png"
+              alt="pricing"
+              width={860}
+              height={722}
+            />
+          </figure>
         </div>
       </div>
       <div className="row">
-        <div className="container">
+        <div className="container__no-cap">
           <div className="plan__features--wrapper">
             <div className="plan__features">
               <figure className="plan__features--icon">
